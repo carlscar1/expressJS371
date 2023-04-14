ecc88ec9550936ce1b78b54c62b66eee6bd6821a
carlscar1 <79236208+carlscar1@users.noreply.github.com>
2023-04-13 22:00:27 -0400

    sends random word!

--- my-server.ts
+++ my-server.ts
@@ -1,9 +1,19 @@
 import express, {Application, Request, Response} from "express";
+
+let storedWords: Array<string> = ["apple", "shade", "watch", "agree", "power", "piece", "bloom", "plead", "sorry", "spoon", "bunny", "pause", "audio", "dream"]
+
+function getRandomInt() {
+    return Math.floor(Math.random() * storedWords.length);
+  }
+
+var index:number = getRandomInt()
+let currentWord = storedWords[index]
 const app:Application = express();
 const PORT = process.env.PORT ?? 8000;    // Allow dynamic PORT setting (Heroku)
+
 // Define GET endpoint(s)
 app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World!");
+  res.send(currentWord);
 });
 app.listen(PORT, () => {
   console.log(`Server is listening to port ${PORT}`);
